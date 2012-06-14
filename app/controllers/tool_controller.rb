40e3dee1da009224d9247d71d15c4b05bbe4ef60
alter <alter.pub@gmail.com>
2012-06-14 23:34:30 +0400

    added login page with simple form

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -1,10 +1,19 @@
 class ToolController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+    end
   end
 
   def activation
+    respond_to do |format|
+      format.html
+    end
   end
 
   def sending
+    respond_to do |format|
+      format.html
+    end
   end
 end
