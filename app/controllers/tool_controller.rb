be639dfa57f68eb5430850c4e66b3e58f4c8a1e1
alter <alter.pub@gmail.com>
2012-06-18 17:48:19 +0400

    small fixes

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -2,12 +2,21 @@ class ToolController < ApplicationController
   def index
   end
 
+  def gmtoolConnection(ip="192.168.198.74", port="8088", user="vzlobin", password="1")
+    apiurl = "http://#{ip}:#{port}/gametool/hessian/account.api"
+    @gmtool = Hessian2::HessianClient.new(apiurl)
+    @gmtool.user = user
+    @gmtool.password = password
+  end
+
+  def billingConnection(ip="192.168.198.67", port="8080")
+    apiurl = "http://#{ip}:#{port}/billingapi"
+    @billing = Hessian2::HessianClient.new(apiurl)
+  end
+
   def activation
     @accountName = params[:accountName]
-    master_gmtool_apiurl = 'http://192.168.198.74:8088/gametool/hessian/account.api'
-    @client = Hessian2::HessianClient.new(master_gmtool_apiurl)
-    @client.user = 'vzlobin'
-    @client.password = '1'
+    gmtoolConnection()
     
     respond_to :html
   end
@@ -17,5 +26,11 @@ class ToolController < ApplicationController
     @shardName   = params[:shardName]
     @avatarId    = params[:avatarId]
     @code        = params[:code]
+
+    gmtoolConnection()
+    billingConnection()
+
+    
+
   end
 end
